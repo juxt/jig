e1d337b7063ed4251bfffda2bf7ef271a3cbbc87
Malcolm Sparks <malcolm@congreve.com>
2013-09-18 15:59:08 +0100

    Clojure core.async has been released.

--- project.clj
+++ project.clj
@@ -62,7 +62,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [core.async "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.async "0.1.222.0-83d0c2-alpha"]
                  ;; Tracing
                  [org.clojure/tools.trace "0.7.5"]
                  ;; Logging
