3dc4617b4f6807793977dd94fa1a95df0d6a817b
Malcolm Sparks <malcolm@juxt.pro>
2013-12-19 11:12:37 +0000

    don't know what happened there, must be a rogue copy

--- examples/snake/src-cljs/snake/cljs/app.cljs
+++ examples/snake/src-cljs/snake/cljs/app.cljs
@@ -1,5 +1,26 @@
-(ns snake.board)
+(ns snake.cljs.app
+    (:require [clojure.string :as s]
+              [snake.cljs.home :as home]
+              clojure.browser.repl)
+    (:require-macros [dommy.macros :refer [sel sel1]]))
 
-(def block-size-px 10)
+(def default-hash "#/")
 
-(def board-size 40)
+(defn- bind-hash [!hash]
+  (letfn [(on-hash-change []
+            (reset! !hash (.-hash js/location)))]
+    
+    (set! (.-onhashchange js/window) on-hash-change)
+    
+    (when (s/blank? (.-hash js/location))
+      (set! (.-hash js/location) default-hash))
+    
+    (on-hash-change)))
+
+(set! (.-onload js/window)
+      (fn []
+        (let [!hash (atom nil)]
+          (home/watch-hash! !hash)
+          (bind-hash !hash))))
+
+
