52f07b3191bafc5ce9073830515e10f53de5830d
Malcolm Sparks <malcolm@juxt.pro>
2014-01-10 11:19:40 +0000

    Be explicit about cljs-builder's dependency on org.clojure/java.classpath

--- extensions/cljs-builder/project.clj
+++ extensions/cljs-builder/project.clj
@@ -16,4 +16,5 @@
   :url "https://github.com/juxt/jig/tree/master/extensions/cljs-builder"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "0.0-2138"]])
+  :dependencies [[org.clojure/clojurescript "0.0-2138"]
+                 [org.clojure/java.classpath "0.2.1"]])
