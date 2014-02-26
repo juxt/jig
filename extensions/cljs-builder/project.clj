fe389d5689e5cd0f6b38c9fd903416ae8e14dddd
Malcolm Sparks <malcolm@juxt.pro>
2014-02-26 10:13:12 +0000

    Update clojurescript to 0.0-2173
    For om 0.5.0 compatibility.

--- extensions/cljs-builder/project.clj
+++ extensions/cljs-builder/project.clj
@@ -19,5 +19,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[jig/protocols ~version]
-                 [org.clojure/clojurescript "0.0-2138"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/java.classpath "0.2.1"]])
